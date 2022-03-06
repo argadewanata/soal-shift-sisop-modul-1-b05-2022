f99d29fdf4de8f8125971e4a12494502c8d7f20e
Rere Arga Dewanata <rerearga@gmail.com>
2022-03-06 08:03:05 +0000

    Solve number 1C

--- soal1/main.sh
+++ soal1/main.sh
@@ -1,1 +1,53 @@
+#!/bin/bash
 
+echo "Login Mode"
+read  username
+read -r -s password
+
+path= pwd 
+date=$(date '+%Y-%m-%d %H:%M:%S')
+folder_date=$(date '+%Y-%m-%d')_$username
+
+if grep -qi ${username} user.txt && grep -qi  ${password} user.txt ;
+then
+    echo "LOGIN: INFO User ${username} logged in"
+    echo "${date} LOGIN: INFO User ${username} logged in" >>  log.txt
+    echo "dl or att?"
+    read command
+    if [[ ${command} == att ]];
+    then 
+        awk -v username="$username" 'BEGIN {count=0} $6 == username || $10 == username {count++} END {print (count)}' log.txt
+    elif [[ ${command} == dl ]]; 
+    then 
+        echo "How many pictures  do you want to download?"
+        read  n
+        if [[ ! -f "$folder_date.zip" ]]
+        then
+            echo "Zipping"
+            mkdir $folder_date
+            count=0
+            for(( i=1; i<=$n; i++ ))
+            do
+                wget https://loremflickr.com/320/240 $folder_date/PIC_$i.jpg
+            done 
+            zip -P $password -r $folder_date.zip $folder_date/
+	        rm -rf $folder_date
+        else
+            echo "Unzipping Mode"
+            unzip -P $password $folder.zip
+            rm $folder.zip
+            count=$(find $folder -type f | wc -l)
+            for(( i=1; i<=$n; i++ ))
+            do
+               wget https://loremflickr.com/320/240 $folder_date/PIC_$i.jpg
+            done 
+            zip -P $password -r $folder.zip $folder_date/
+	        rm -rf $folder_date
+        fi
+    else
+        echo "Wrong Command"
+    fi
+else
+    echo "LOGIN: ERROR Failed login attempt on user ${username}"
+    echo "${date} LOGIN: ERROR Failed login attempt on user ${username}" >>  log.txt
+fi
--- soal1/register.sh
+++ soal1/register.sh
@@ -1,41 +1,38 @@
 #!/bin/bash
 
-
+path=pwd
 echo "Register Mode"
 
-read username
+read  username
 
-read -r -s user_password
+read -r -s password
 date=$(date '+%Y-%m-%d %H:%M:%S')
 
-
-
-if grep -qi ${username} /home/noob/Documents/sisop/Mod1/soal1user.txt
+if grep -qi ${username} user.txt;
 then
-    echo "Failed!!"
-    echo "Error: User Already Exist"
-    echo "${date} Error: User Already Exist" >> log.txt
+    echo "REGISTER: Error User already exist"
+    echo "${date} REGISTER: Error User already exist" >> log.txt
 else
-    if [[ ${username} != ${user_password} ]]; then
-        if [[ ${user_password} =~ [A-Za-z0-9]{8,} ]]; then
-            if [[ ${user_password} =~ [A-Z] && ${user_password} =~ [a-z] && ${user_password} =~ [0-9] ]]; then
-                echo "Success Registered"
-                echo "${username}" >> /home/noob/Documents/sisop/Mod1/soal1/user.txt
-                echo "${user_password}" >> /home/noob/Documents/sisop/Mod1/soal1user.txt
-                echo "${date} ${username} registered successfully" >> log.txt
+    if [[ ${username} != ${password} ]]; then
+        if [[ ${password} =~ [A-Za-z0-9]{8,} ]]; then
+            if [[ ${password} =~ [A-Z] && ${password} =~ [a-z] && ${password} =~ [0-9] ]]; then
+                echo "REGISTER: INFO User ${username} registered successfully"
+                echo "${username}" >> user.txt
+                echo "${password}" >> user.txt
+                echo "${date} REGISTER: INFO User ${username} registered successfully" >> log.txt
             else
                 echo "Failed!!"
                 echo "Error: Password must constaint from uppercase, lowercase, and numeric"
-                echo "${date} Error: Password must constaint from uppercase, lowercase, and numeric" >> log.txt
+                echo "${date} REGISTER: Error Password must constaint from uppercase, lowercase, and numeric" >> log.txt
             fi
         else
             echo "Failed!!"
             echo "Error: Password must constaint minimal 8 characters"
-            echo "${date} Error: Password must constaint minimal 8 characters" >> log.txt
+            echo "${date} REGISTER: Error Password must constaint minimal 8 characters" >> log.txt
         fi
     else
         echo "Failed!!"
         echo "Error: Password must not same the username"
-        echo "${date} Error: Password must not same the username" >> log.txt
+        echo "${date} REGISTER: Error Password must not same the username" >> log.txt
     fi
 fi
