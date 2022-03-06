9b85e8ca5a9c08f38575d83081f4956cda179ac6
Miawmiw <fahmimuhazir.205025@mhs.its.ac.id>
2022-03-06 10:51:47 +0000

    commit ulang karena salah update

--- soal3/aggregate_minutes_to_hourly.sh
+++ soal3/aggregate_minutes_to_hourly.sh
@@ -4,16 +4,15 @@
 USER= id -u -n
 echo Hy $USER
 
-
 #buat variabel biar ezzz
-Date= $(date + '%Y%m%d%H')
-Pattern= "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size"
-DIR_OUT="/home/noob/Documents/sisop/Mod1/soal3/log/metrics_$FILE.log"
+Date=$(date +"%Y%m%d%H")
+Pattern= echo "mem_total,mem_used,mem_free,mem_shared,mem_buff,mem_available,swap_total,swap_used,swap_free,path,path_size"
+DIR_OUT="/home/noob/log/metrics_$Date.log"
 
 #Checkin var of file when it have "mem", used grep
 check_var() {
-	for resources in $($DIR_OUT/log/metrics_2022* | grep $Date)
-	do cat $resources | grep -v mem 
+	for file in $($DIR_OUT/log/metrics* | grep $Date)
+	do cat $file | grep -v mem 
 	done
 }
 
@@ -22,4 +21,3 @@ check_var() {
 
 #Input without replace oldes value
 echo $Pattern  >> $DIR_OUT
-
